f84b4aafacd4e79a1a7932d87302a9401b66e3f6
linjianghe <linjianghe@tencent.com>
2018-06-02 10:54:35 +0800

    feat: 新增 demo_03

---
+++ test/data/demo_03/matman.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  ROOT_PATH: __dirname,
+  clientScriptBuildPath: path.join(__dirname, 'dist-client-script'),
+  clientScriptMatch: /crawlers\/.*\.js$/,
+  entry: {
+    'page_rule/crawlers/get-page-info': path.join(__dirname, 'e2e_test/targets/page_rule/crawlers/get-page-info.js')
+  }
+};
