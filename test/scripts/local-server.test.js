902f363c88bddb6dbcd4bdca6ae966edc5cf450a
linjianghe <linjianghe@tencent.com>
2018-06-11 15:47:41 +0800

    refactor: 校验元素

--- test/scripts/local-server.test.js
+++ test/scripts/local-server.test.js
@@ -17,5 +17,26 @@ describe('local-server', () => {
     it('should return array and length is 2', () => {
       expect(data).to.be.a('array').and.have.lengthOf(2);
     });
+
+    it('should exist name_demo_basic', () => {
+      let filterResult = data.filter(item => item.name === 'name_demo_basic');
+      let target = filterResult[0];
+
+      expect(filterResult).to.be.a('array').and.have.lengthOf(1);
+      expect(target.mockModuleList).to.be.a('array').and.have.lengthOf(5);
+      expect(target.config).to.eql({
+        'name': 'name_demo_basic',
+        'route': '/cgi-bin/a/b/demo_basic',
+        'description': 'description_demo_basic',
+        'disable': false,
+        'defaultModule': 'success_1',
+        'activeModule': 'success_1',
+        'method': 'get',
+        'priority': 0,
+        'tags': [
+          '全部'
+        ]
+      });
+    });
   });
 });
