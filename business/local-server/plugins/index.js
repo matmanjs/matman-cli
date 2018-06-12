9887f084e4c48bba65a751526540d10db9ad8420
linjianghe <linjianghe@tencent.com>
2018-06-12 22:59:00 +0800

    refactor: 将 mocker 相关的代码移至单独的目录下

--- business/local-server/plugins/index.js
+++ business/local-server/plugins/index.js
@@ -1,16 +1,8 @@
 const mockerRouter = require('./mocker/router');
-// const reporterRouter = require('../../plugins/reporter/router');
-// const stubRouter = require('../../plugins/stub/router');
 
-module.exports = (router, mockerParser) => {
+module.exports = (router, entry) => {
 
   // 初始化 mocker
-  mockerRouter(router, mockerParser);
-
-  // 初始化 reporter
-  // reporterRouter(router, handlerParser);
-  //
-  // // 初始化 stub
-  // stubRouter(router, handlerParser);
+  mockerRouter(router, entry);
 
 };
