aca3ef3737bb78d348c230ceeda1326e2ae6ac83
linjianghe <linjianghe@tencent.com>
2018-06-20 13:18:52 +0800

    refactor: 修复智能获取 entry 时的bug #89

--- business/e2e-build/index.js
+++ business/e2e-build/index.js
@@ -1,10 +1,22 @@
 const matman = require('../matman');
 
-function getClientScriptHandler(rootPath, buildPath, regMatch) {
+/**
+ * @function createProdConfig
+ * @desc     创建用于生产环境中的webpack打包配置
+ *
+ * @param {Object} params
+ * @param {String} params.rootPath 项目根目录
+ * @param {String} params.e2ePath 端对端项目的根目录
+ * @param {RegExp} params.clientScriptMatch 正则表达式，用于匹配路径中那些文件是 client script
+ * @param {String} params.clientScriptBuildPath 输出打包后的 client script 的路径
+ * @example
+ */
+function getClientScriptHandler(params) {
   const clientScript = new matman.ClientScript({
-    rootPath: rootPath,
-    buildPath: buildPath,
-    regMatch: regMatch
+    rootPath: params.rootPath,
+    basePath: params.e2ePath,
+    buildPath: params.clientScriptBuildPath,
+    regMatch: params.clientScriptMatch
   });
 
   return clientScript;
