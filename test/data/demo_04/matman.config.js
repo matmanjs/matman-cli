6026e06fa813ec2b5460ccbd990e2d64e005d74c
helinjiang <me@helinjiang.com>
2018-06-26 23:31:13 +0800

    feat: 新增 demo_04

---
+++ test/data/demo_04/matman.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  // rootPath: __dirname,
+  // buildPath: path.resolve(__dirname, './build'),
+  mockServerPath: path.resolve(__dirname, '../fixtures/mock_server/mockers'),
+  // clientScriptBuildPath: path.join(__dirname, 'dist-client-script'),
+  // clientScriptMatch: /crawlers\/.*\.js$/,
+  // entry: {
+  //   'page_rule/crawlers/get-page-info': path.join(__dirname, 'e2e_test/targets/page_rule/crawlers/get-page-info.js')
+  // }
+};
