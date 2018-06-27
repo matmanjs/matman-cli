8e9ccabc286823c806a9f688ff79dd6e3c8fabda
linjianghe <linjianghe@tencent.com>
2018-06-27 09:48:13 +0800

    refactor: start 命令只是将 config 的绝对路径传递过去

--- business/local-server/index.js
+++ business/local-server/index.js
@@ -1,7 +1,14 @@
 const run = require('./run');
 
-function startServer(opts) {
-  run(opts);
+function startServer(configAbsolutePath, cwd) {
+  let matmanConfig = require(configAbsolutePath);
+
+  // 如果不定义 rootPath，则默认取 process.cwd()
+  if (!matmanConfig.rootPath) {
+    matmanConfig.rootPath = cwd;
+  }
+
+  run(matmanConfig);
 }
 
 module.exports = {
