c93ddc0cb6f69de234f9b141a5b93f6808888881
linjianghe <linjianghe@tencent.com>
2018-06-27 10:03:08 +0800

    feat: start 命令增加 --dev 参数，来使用 node 直接启动服务

--- business/local-server/index.js
+++ business/local-server/index.js
@@ -1,11 +1,18 @@
 const run = require('./run');
 
-function startServer(configAbsolutePath, cwd) {
+/**
+ * 启动服务
+ *
+ * @param {Boolean} isDev 是否为开发者模式
+ * @param {String} configAbsolutePath matman.config.js 文件的绝对路径
+ * @param {String} [cwd] 项目启动的目录，默认为 process.cwd()
+ */
+function startServer(isDev, configAbsolutePath, cwd) {
   let matmanConfig = require(configAbsolutePath);
 
   // 如果不定义 rootPath，则默认取 process.cwd()
   if (!matmanConfig.rootPath) {
-    matmanConfig.rootPath = cwd;
+    matmanConfig.rootPath = cwd || process.cwd();
   }
 
   run(matmanConfig);
